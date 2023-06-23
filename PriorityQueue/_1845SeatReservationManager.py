49a4fde70f1c581edb865bd3798b207b1e1037b7
SitongChe <chesitong@126.com>
2023-06-23 00:50:06 -0700

    comment

--- PriorityQueue/_1845SeatReservationManager.py
+++ PriorityQueue/_1845SeatReservationManager.py
@@ -4,7 +4,7 @@ class SeatManager:
 
     def __init__(self, n: int):
         self.minHeap = [i for i in range(1,n+1)]
-        # no need to heapify is already sorted
+        # no need to heapify if already sorted
         #heapq.heapify(self.minHeap)
         
 
@@ -21,3 +21,5 @@ class SeatManager:
 # obj = SeatManager(n)
 # param_1 = obj.reserve()
 # obj.unreserve(seatNumber)
+
+# In the context of a min-heap, it rearranges the elements of the list such that the smallest element becomes the root, and for any node at index i, the values of its children at indices 2*i+1 and 2*i+2 are greater than or equal to the value at index i.
